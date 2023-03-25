72e91403086a5177e406834a39cf8758028741a2
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-03-25 23:06:30 +0600

    Created and organised files and folders

---
+++ Onboarding Screen/Onboarding_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onboarding_ScreenApp.swift
+//  Onboarding Screen
+//
+//  Created by Md Abir Hossain on 25/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Onboarding_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Onboarding Screen/Screens/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Onboarding Screen
+//
+//  Created by Md Abir Hossain on 25/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
